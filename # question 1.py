2a7171495f6f6e83da8c2e69394b60ceb590cb96
Saurav Kumar Kamat <akelasaurav766@gmail.com>
2025-03-26 15:43:21 +0530

    Add files via upload

---
+++ # question 1.py
@@ -0,0 +1,66 @@
+# question 1
+# for i in range(1,100):
+#     print(i,end =" ")
+
+    # question 2
+# for i in range(1,11):
+#     print(i*2,end=" ")
+
+# queation 3     
+for i in range(1,11):
+    if i%2==0:
+      print(i, end=" ")
+ 
+# queation 4
+# total=0
+# for i in range(1,11):
+#     total  += i
+#     print(total)
+
+# question 5
+# worde= "akela"
+# for i in range(len(worde)-1,-1,-1):
+#     print(worde[i])
+
+# question 6
+# vowels="aeiou"
+# worde="akela saurav"
+# count=0
+# for char in worde:
+#     if char in vowels:
+#         count +=1
+# print(f"Total vowels in {worde} is {count}")
+
+# question 7
+# a=0
+# b=1
+# print(a,b, end=" ")
+# for _ in range(9):
+#     next_term= a+b
+#     print(next_term,end=" ")
+#     a,b=b,next_term
+
+# question 8
+# n=5
+# fact=1
+# for i in range(1,n+1):
+#   fact *=i
+# print(f"factorial of {n} is {fact}")
+
+# question 9
+# num=1
+# is_prime=True
+# for i in range(2,int(num**0.5)+1):
+#     if num%i==0:w
+#         is_prime=False
+#         break
+#     if is_prime and num>1:
+#         print(num," is a prime number")
+#     else:
+#         print(num," is not a prime number")
+
+# f=open("simple.txt","a")
+# data=f.write('akela saurav kumar\n haii saurav')
+# print(type(data))
+# f.close()
+
